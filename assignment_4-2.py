9be55ff2b4d939b87bd92a9da3174bae2695b92d
ksnts <santosjohnkyllem@gmail.com>
2021-12-29 00:38:10 +0800

    Second draft

--- assignment_4-2.py
+++ assignment_4-2.py
@@ -1,7 +1,14 @@
+print("Note that: Apples: 20 pesos each, Orange: 25 pesos each")
+
 def price_code():
     orangeP=25*buyO
     appleP=20*buyA
     total1=orangeP+appleP
     return total1
 
+def main_code():
+    orange1=int(input("Input amount of oranges: "))
+    apple1=int(input("Input amount of apples: "))
 
+
+    
