38ed0a01d331e5a503ed8ddbf8b12df8fdfda2db
ksnts <santosjohnkyllem@gmail.com>
2021-12-29 00:57:19 +0800

    Final code

--- assignment_4-2.py
+++ assignment_4-2.py
@@ -1,14 +1,19 @@
 print("Note that: Apples: 20 pesos each, Orange: 25 pesos each")
-
-def price_code():
-    orangeP=25*buyO
-    appleP=20*buyA
-    total1=orangeP+appleP
-    return total1
-
+#main code for inputting data 
 def main_code():
     orange1=int(input("Input amount of oranges: "))
     apple1=int(input("Input amount of apples: "))
+    total=price_code(orangeP=orange1, appleP=apple1)
+    print(f"Total amount is: {total} pesos")
+
+#function for computing the total price
+def price_code(orangeP, appleP):
+    orangeA=25*orangeP
+    appleA=20*appleP
+    total1=orangeA+appleA
+    return total1
+#call
+main_code()
 
 
     
