fe33adc65448d62fb7dd49f2e5663316dd35d40b
ksnts <santosjohnkyllem@gmail.com>
2021-12-29 00:29:30 +0800

    Code complete

--- assignment_4.py
+++ assignment_4.py
@@ -1,7 +1,12 @@
+#main code for gathering and returning data
 def main_code():
     name1=input("Input your name: ")
     age1=input("Input your age: ")
     add1=input("Input your address: ")
     return name1, age1, add1
+#move all inputs to one function
+input1, input2, input3 = main_code()
 
-    
+
+#print output
+print(f"Hi, my name is {input1}. I am {input2} years old and I live in {input3}.")
