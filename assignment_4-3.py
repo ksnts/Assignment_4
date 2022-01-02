c4ab6c686fb81972e5ed9eb046b65885393e8d41
ksnts <santosjohnkyllem@gmail.com>
2022-01-02 22:36:11 +0800

    Second draft

--- assignment_4-3.py
+++ assignment_4-3.py
@@ -2,3 +2,10 @@ def price():
     money1=int(input("Input amount of money: "))
     appleP=int(input("Input apple price: "))
     return money1, appleP
+
+def main_code(money1, appleP):
+    total1=money1//appleP
+    total2=money1%appleP
+    print(f"You can buy amount of apples: {total1}")
+    print(f"Your change is: {total2}")
+
